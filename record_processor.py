1fb2385736f063960a93cef349549c2bbf87f120
Chris Mac <cmac@rrc.ca>
2025-02-26 14:13:47 -0600

    rename main script file

--- record_processor.py
+++ record_processor.py
@@ -1,13 +1,1 @@
-# Script to process data records
-
-header_record = "First Name, Last Name, Age, Height, Sex"
-test_records = [
-    "Bob, Newhart, 78, 175, M",
-    "Stacy, DUNCAN, 22, 189, F",
-    "marilyn, Manson,, 176, Male"
-]
-
-for record in test_records:
-    # run validate on each
-    pass
-
+# Classes for processing records
